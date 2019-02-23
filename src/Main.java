baa25fe1a73211ed8c7a6f61d4fd1fe3193175f7
Jeremy <piranna56@yahoo.fr>
2019-02-23 18:14:46 +0100

    add Decorator

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,18 @@
 
+import decorator.GoodOlCheapoTVA;
 import programme.GoodOlCheapo;
 
 public class Main {
 	
 	public static void main(String[] args) {
+	
+		//Ancienne classe
 		GoodOlCheapo goCheap = new GoodOlCheapo();
 		System.out.println("White Haze : "+ goCheap.getBestOffer("White Haze").price);
+		
+		//Application design pattern Decorator
+		GoodOlCheapoTVA goCheapTVA = new GoodOlCheapoTVA(goCheap);
+		System.out.println("White Haze whith TVA : "+ goCheapTVA.getBestOffer("White Haze").price);
 	}
 
 }
--- src/decorator/GoodOlCheapoTVA.java
+++ src/decorator/GoodOlCheapoTVA.java
@@ -1,17 +1,43 @@
 package decorator;
 
+import java.util.Collection;
+import java.util.Iterator;
+
 import programme.ElCheapo;
 import programme.Offer;
+import programme.Product;
+import programme.ProductInter;
+import programme.VendorInter;
+import programme.Vendors;
 
 public class GoodOlCheapoTVA extends ElCheapoDecorator {
+	
+	private Collection<VendorInter> mVendors = Vendors.getVendors();
 
 	public GoodOlCheapoTVA(ElCheapo composant) {
 		super(composant);
 	}
 	
 	protected Offer getBestOfferTVA(String productName) {
-		//Offer offer = new Offer(double price, Vendor vendor, ProductCategory category);
-		return this.getBestOffer(productName);
+		Iterator<VendorInter> it = mVendors.iterator();
+		Iterator<VendorInter> it2 = mVendors.iterator();
+		it2.next();
+		Offer offer = null;
+		while(it.hasNext()) {
+			VendorInter vendor = it.next();
+			double price = vendor.getPrice(productName);
+			
+			while(it2.hasNext()) {
+				VendorInter vend2 = it2.next();
+				double price2 = vend2.getPrice(productName);
+				if(price >= price2) {
+					price = vend2.getPrice(productName);
+					ProductInter prod = vend2.getProduct(productName);
+					offer = new Offer(price, vendor, ((Product)prod).getCategory());
+				}
+			}
+		}
+		return offer;
 	}
 	
 	@Override
